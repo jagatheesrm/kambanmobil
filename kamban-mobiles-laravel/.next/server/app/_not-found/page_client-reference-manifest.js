aa6ab63d74032066f0adb4ba58dd6c4aa063de6a
Jagathees waran <61326194+jagatheesrm@users.noreply.github.com>
2025-07-14 23:49:02 +0530

    Updated app-build-manifest.json

---
+++ kamban-mobiles-laravel/.next/server/app/_not-found/page_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/_not-found/page"]={"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"/home/project/kamban-mobiles-laravel/":[],"/home/project/kamban-mobiles-laravel/app/_not-found/page":[]}}
